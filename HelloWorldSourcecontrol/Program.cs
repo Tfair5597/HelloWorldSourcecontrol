580f5404584759289f88b761d3958d175c30014c
Jasper-Lawliet <Tfair5597@users.noreply.github.com>
2018-01-03 14:51:32 -0800

    Fixed a typo.

--- HelloWorldSourcecontrol/Program.cs
+++ HelloWorldSourcecontrol/Program.cs
@@ -8,7 +8,7 @@ namespace HelloWorldSourcecontrol
         {
             Console.WriteLine("Hello to the world, most common thing ever.");
             Console.WriteLine("practicing source control...");
-            Console.WriteLine("Will in notice that we made a change?");
+            Console.WriteLine("Will it notice that we made a change?");
             Console.WriteLine("it seems to have noticed. Good.");
             Console.ReadKey();
         }
