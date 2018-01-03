3a83dfc6673c0bbce0f0d13c7dc9c604eb0ec84b
Timothy Fair <tfair@CIT.local>
2018-01-03 14:40:22 -0800

    Add project files.

---
+++ HelloWorldSourcecontrol/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorldSourcecontrol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello to the world, most common thing ever.");
+            Console.WriteLine("practicing source control...");
+            Console.ReadKey();
+        }
+    }
+}
